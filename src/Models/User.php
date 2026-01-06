b05cc929419e5bc47819d8d5275572d6c95e2e17
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-01-06 16:17:44 +0100

    feat: Model user

---
+++ src/Models/User.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Src\Models;
+
+class User
+{
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+    private $created_at;
+    private $updated_at;
+
+    /**
+     * Summary of __construct
+     * @param mixed $id
+     * @param mixed $name
+     * @param mixed $email
+     * @param mixed $password
+     * @param mixed $created_at
+     * @param mixed $updated_at
+     */
+    public function __construct($id, $name, $email, $password, $created_at, $updated_at)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    /*
+     *getters of User Model.
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+
+    /*
+     *setters of User Model.
+     */
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+}
+
